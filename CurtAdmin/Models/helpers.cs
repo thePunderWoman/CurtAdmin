e72da9dfd409a464c7699f72ac58bb02cce4a848
CURT Manufacturing <aninneman@curtmfg.com>
2012-11-26 08:10:53 -0600

    started to remove some conflicts

---
+++ CurtAdmin/Models/helpers.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Mail;
+
+
+namespace CurtAdmin.Models
+{
+    public class helpers
+    {
+        // helpers class is used to extend functionality to the CURT Admin project by including methods that are commonly used
+
+        public static void SendEmail(string email, string subject, string htmlBody, bool isBodyHTML = false)
+        {
+            try
+            {
+                SmtpClient SmtpServer = new SmtpClient();
+                MailMessage mail = new MailMessage();
+                mail.To.Add(email);
+                mail.Subject = subject;
+                mail.IsBodyHtml = isBodyHTML;
+                mail.Body = htmlBody;
+                SmtpServer.Send(mail);
+            }
+            catch (Exception e)
+            {
+                throwException("Error Sending Email" + e.Message);
+            }
+        }
+
+
+        public static void throwException(string msg)
+        {
+            // plan to add more functionality later.
+            throw new Exception(msg);
+        }
+
+
+
+
+
+    }
+}
---
+++ CurtAdmin/Scripts/admin/webPropertiesEnabled.js
@@ -0,0 +1,28 @@
+﻿$(document).ready(function () {
+
+    $('table').dataTable({
+        "bJQueryUI": true
+    });
+
+
+    $('.isEnabled').live('click', function () {
+        var record_id = $(this).attr('id').split(':')[1];
+        set_isEnabled(record_id);
+    });
+
+});
+
+
+/*
+* This function is going to make an AJAX call to the controller and set the isEnabled field.
+* @param record_id: Primary Key for web property
+*/
+function set_isEnabled(record_id) {
+    $.get('/Customers/SetWebPropertyStatus', { 'record_id': record_id }, function (response) {
+        if (response != '') {
+            showMessage(response);
+        } else {
+            showMessage("The Web Property's status has been updated.");
+        }
+    });
+}
